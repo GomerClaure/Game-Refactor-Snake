c4b89e265fa922f6184aefad8390287631b04715
Gomer Claure <gomerclaureb@gmail.com>
2023-03-07 10:50:46 -0400

    Empezando la refactorizacion

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.zetcode.Snake;
+import com.zetcode.Board;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame ex = new Snake();
+        ex.setVisible(true);    }
+}
